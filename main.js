bd434639f5b11f7f2b97ee0242eee4a91582b91b
Souheil Kooli <kooli_shouheil@yahoo.fr>
2020-11-23 08:51:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//** word count 
+//**  const countWords = str => str.split(' ').length;
+
+function WordCount(str) { 
+    return str.split(" ").length;
+  }
+  
+  console.log(WordCount("Testing one two test"));
+
+
+  //** */
+
+const length = ({ length }) => length;
+
+const countVowels = str => str.match(/[aeiou]/g).length;
+
